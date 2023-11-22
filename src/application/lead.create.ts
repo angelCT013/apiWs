2ca34f42a60cb33d5e675b1e3ef86c22b47dd862
angelCT013 <angelct013@gmail.com>
2023-11-22 10:28:05 -0800

    first commit

---
+++ src/application/lead.create.ts
@@ -0,0 +1,41 @@
+import LeadExternal from "../domain/lead-external.repository";
+
+export class LeadCreate {
+  private leadExternal: LeadExternal;
+    constructor(respositories: [ LeadExternal]) {
+    const [leadExternal] = respositories;
+    this.leadExternal = leadExternal;
+  }
+
+  public async sendMessageAndSave({
+    message,
+    phone,
+  }: {
+    message: string;
+    phone: string;
+  }) {
+    // console.log(phone);
+    
+    const resultados: string[] = []; // Ajusta según tu caso, asumo que responseExSave.id es de tipo string
+    const arrayPhone: string[] = Array.isArray(phone) ? phone : [phone]; // Convierte a un arreglo si no lo es
+    
+    await Promise.all(
+      arrayPhone.map(async (phoneNumber) => {
+        const responseExSave = await this.leadExternal.sendMsg({ message, phone: phoneNumber });
+        if(responseExSave.id){
+          resultados.push(phoneNumber);
+
+        }
+      })
+    );
+    return {
+      "success": true,
+      "data":{
+        "NumerosEnviados":resultados
+      },
+      "message":"Mensaje Enviado Correctamente"
+    };
+
+  }
+
+}
---
+++ src/domain/lead.ts
@@ -0,0 +1,16 @@
+import { v4 as uuid } from "uuid";
+
+export class Lead {
+  readonly uuid: string;
+  readonly message: string;
+  readonly phone: string;
+
+  constructor({ message, phone }: { message: string; phone: string }) {
+    this.uuid = uuid();
+    this.message = message;
+    this.phone = phone;
+    // console.log(message);
+    // console.log(phone);
+    
+  }
+}
