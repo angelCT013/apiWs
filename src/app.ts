4f379166f9aa7af9ef657395081de4d2b0c09fbd
angelCT013 <angelct013@gmail.com>
2024-03-15 10:09:30 -0700

    comentando json

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,10 @@ import { Whatsapp } from "./infrastructure/websocket/whatsapp";
 import { readFileSync } from "fs";
 import https from "https"; 
 
-
-
-
-
 const port = process.env.PORT || 3000
 const app = express()
 app.use(cors())
-app.use(express.json())
+// app.use(express.json())
 app.use(express.static('tmp'))
 app.use(express.json({ limit: '50mb' }));
 
